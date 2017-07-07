38de93b20e215d01a0b6e0997b1e22d090db79bd
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-07 12:14:05 +0530

    Create Apps.sql

---
+++ Apps.sql
@@ -0,0 +1,11 @@
+USE [sstdb]
+GO
+
+INSERT INTO [dbo].[Apps]
+           ([Title]
+           ,[AppName]
+           ,[URL])
+     VALUES
+           ('Employee Resources','My SST CEU Resources','http://www.shiningstartherapy.com/opdp-library/')
+		   
+GO
