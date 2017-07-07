c7c8bc15bae4cc1ecdd0fd944807545a1233b809
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-07 12:15:56 +0530

    Create EmailInAttachments.sql

---
+++ EmailInAttachments.sql
@@ -0,0 +1,10 @@
+USE [sstdb]
+GO
+
+INSERT INTO [dbo].[EmaillnAttachments]
+           ([EmailId]
+           ,[Attachment])
+     VALUES
+           (2,'System.Net.Mail.AttachmentCollection')
+		   
+GO
