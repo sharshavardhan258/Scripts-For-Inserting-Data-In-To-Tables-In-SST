578bae42be80c80a38faddb180e036030f8ee6a4
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-07 12:17:33 +0530

    Create My Products.sql

---
+++ My Products.sql
@@ -0,0 +1,11 @@
+USE [sstdb]
+GO
+
+INSERT INTO [dbo].[MyProducts]
+           ([Title]
+           ,[URL]
+           ,[AppName])
+     VALUES
+           ('TherapyBoss1','https://shiningstartherapy.bamboohr.com/home/#','TherapyBoss1')
+		   
+GO
