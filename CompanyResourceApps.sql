e1f3cce8eb51619b8eafdc2dbbf0d2f699ae8081
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-07 12:14:34 +0530

    Create CompanyResourceApps.sql

---
+++ CompanyResourceApps.sql
@@ -0,0 +1,11 @@
+USE [sstdb]
+GO
+
+INSERT INTO [dbo].[CompanResourceApps]
+           ([Title]
+           ,[AppName]
+           ,[URL])
+     VALUES
+           ('Company Resources','Team Meeting ','http://www.gogle.com/company-news/')
+		  
+GO
