e52345753eb77e6d1e3d9dc4c22f9962032a6ad4
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-07 12:15:29 +0530

    Create EmailIn.sql

---
+++ EmailIn.sql
@@ -0,0 +1,11 @@
+USE [sstdb]
+GO
+
+INSERT INTO [dbo].[EmailIn]
+           ([EmailIn]
+           ,[Subject]
+           ,[Body])
+     VALUES
+           ('bhaktavatsal.nedurumalli@gmail.com','SST Push notifications','Push notifications working fine')
+		  
+GO
