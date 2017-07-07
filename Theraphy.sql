2a4c8e15a31d358e4a38d669a886b4ac378af9f0
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-07 12:18:25 +0530

    Create Theraphy.sql

---
+++ Theraphy.sql
@@ -0,0 +1,18 @@
+USE [sstdb]
+GO
+
+INSERT INTO [dbo].[Theraphy]
+           ([Name]
+           ,[TheraphyType]
+           ,[ContactNumber]
+           ,[Address 1]
+           ,[City]
+           ,[ZipCode]
+           ,[State]
+           ,[ServiceArea]
+           ,[CFC]
+           ,[Email])
+     VALUES
+           ('Lokesh','physiotherapy',+918501020553,'Illinoise','Chicago',50006,'Chicago','OT',20,'swaroop1232010@gmail.com')
+		  
+GO
