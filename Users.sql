1ffb1cfffe69c763f095afef9f297c398efbf4e1
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-07 12:19:49 +0530

    Create Users.sql

---
+++ Users.sql
@@ -0,0 +1,13 @@
+USE [sstdb]
+GO
+
+INSERT INTO [dbo].[Users]
+           ([Email]
+           ,[UserName]
+           ,[UserRole]
+           ,[DeviceId]
+           )
+     VALUES
+           ('swaroop1232010@gmail.com','lokesh','Admin','Tab')
+		   
+GO
