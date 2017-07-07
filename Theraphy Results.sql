a327bc02704786be0916693e4b073b1ac3e01286
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-07 12:18:55 +0530

    Create Theraphy Results.sql

---
+++ Theraphy Results.sql
@@ -0,0 +1,13 @@
+USE [sstdb]
+GO
+
+INSERT INTO [dbo].[TheraphyResults]
+           ([FromMail]
+           ,[Body]
+           ,[Subject]
+           ,[City]
+           ,[ZipCode])
+     VALUES
+           ('swaroop1232010@gmail.com','OT need in the area of 60634. Aurora is a city','OT Assessment (Lockport)','Lockport',50006)
+		  
+GO
