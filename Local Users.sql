7f1347181c845c37c83f4bad031268eb34356cda
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-07 12:17:05 +0530

    Create Local Users.sql

---
+++ Local Users.sql
@@ -0,0 +1,11 @@
+USE [sstdb]
+GO
+
+INSERT INTO [dbo].[LocalUsers]
+           ([UserName]
+           ,[Password]
+           ,[Source])
+     VALUES
+           ('Lokesh','lokesh','Social Website')
+		   
+GO
