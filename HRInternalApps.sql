64e0a24f4ea988b11fd793f53f96002d433ecf67
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-07 12:16:24 +0530

    Create HRInternalApps.sql

---
+++ HRInternalApps.sql
@@ -0,0 +1,11 @@
+USE [sstdb]
+GO
+
+INSERT INTO [dbo].[HRInternalApps]
+           ([Title]
+           ,[AppName]
+           ,[URL])
+     VALUES
+           ('TherapyBoss','TherapyBoss','https://shiningstartherapy.bamboohr.com/login.php?r=%2Fhome%2F#')
+		   
+GO
